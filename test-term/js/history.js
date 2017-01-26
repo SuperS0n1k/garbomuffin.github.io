1fdd15a94f2889b217b0aebd7bcf1db61e05ce31
Thomas Weber <tommyweber33@gmail.com>
2017-01-25 21:21:59 -0600

    fix the history

--- test-term/js/history.js
+++ test-term/js/history.js
@@ -6,7 +6,7 @@ var currentHistory = 0;
 function historyChange(x){
   var newCommand = commandHistory[currentHistory + x];
   if (newCommand !== undefined){
-    $("#out").text("$ " + commandHistory[currentHistory + x]);
+    $("#out").text(commandHistory[currentHistory + x]);
     currentHistory = currentHistory + x;
   }
 }
