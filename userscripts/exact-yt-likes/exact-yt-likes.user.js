d5ec77d5e214368c1324c4076265c2d3da4f1f1b
Thomas Weber <tommyweber33@gmail.com>
2017-09-03 16:38:28 -0500

    update exact-yt-likes (still buggy)

--- userscripts/exact-yt-likes/exact-yt-likes.user.js
+++ userscripts/exact-yt-likes/exact-yt-likes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Exact YT Likes
 // @namespace    https://garbomuffin.bitbucket.io/userscripts/exact-yt-likes
-// @version      1.0
+// @version      1.1
 // @description  Exact likes on YouTube with the new layout
 // @author       GarboMuffin
 // @match        https://www.youtube.com/watch*
@@ -16,17 +16,28 @@
     return i.match(/[0123456789,]*/g)[0];
   }
 
-  // give youtube time to load things
-  // maybe i should figure out how to actually do this
-  // document-idle doesn't run late enough
-  setTimeout(function(){
+  function fix(){
     // the first 2 elements with tag name ytd-toggle-button-renderer are the like and dislike buttons
     var buttons = document.getElementsByTagName("ytd-toggle-button-renderer");
+    if (buttons.length === 0){
+      return false;
+    }
+
     var el = [buttons[0], buttons[1]];
 
     for (var i of el){
       var e = i.getElementsByTagName("yt-formatted-string")[0];
       e.textContent = number(e.getAttribute("aria-label"));
     }
-  }, 1000);
+
+    return true;
+  }
+
+  // give youtube time to load things
+  // document-idle doesn't run late enough
+  var interval = setInterval(function(){
+    if (fix()){
+      clearInterval(interval);
+    }
+  });
 })();
