3238eefef9e4ba551c0c4b9bccc832134ca3da93
Thomas Weber <tommyweber33@gmail.com>
2017-09-09 00:29:02 -0500

    add bring-back discuss
    BILLY MAYS HERE

---
+++ userscripts/bring-back-discuss/bring-back-discuss.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         bring back discuss
+// @namespace    https://garbomuffin.bitbucket.io/userscripts/bring-back-discuss/
+// @version      1.0
+// @description  some people wanted the discuss button back or something???
+// @author       GarboMuffin
+// @match        https://scratch.mit.edu/*
+// ==/UserScript==
+
+(function(){
+  "use strict";
+
+  var tipsEl = document.getElementsByClassName("link tips")[0];
+  var modernDesign = !!tipsEl;
+
+  if (modernDesign){
+    var tipsLink = tipsEl.getElementsByTagName("a")[0];
+    var tipsText = tipsEl.getElementsByTagName("span")[0];
+
+    tipsLink.href = "/discuss/";
+    tipsText.textContent = "Discuss";
+  }else{
+    var siteNav = document.getElementsByClassName("site-nav")[0];
+    var navEntry = siteNav.getElementsByTagName("li")[2];
+    var oldTipsLink = navEntry.getElementsByTagName("a")[0];
+
+    oldTipsLink.href = "/discuss/";
+    oldTipsLink.textContent = "Discuss";
+  }
+})();
