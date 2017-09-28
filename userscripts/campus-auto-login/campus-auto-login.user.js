161557d26fe73f5f6a38e77207252cadb0a67f2f
Thomas Weber <tommyweber33@gmail.com>
2017-09-28 17:38:08 -0500

    campus auto login v3.1

--- userscripts/campus-auto-login/campus-auto-login.user.js
+++ userscripts/campus-auto-login/campus-auto-login.user.js
@@ -1,82 +1,159 @@
 "use strict";
-// ==UserScript==
-// @name         Campus Auto Login
-// @version      3.0
-// @description  Auto log-in to campus portal.
-// @author       GarboMuffin
-// @match        https://campus.district112.org/campus/portal/isd112.jsp*
-// @match        https://campus.district112.org/campus/portal/students/isd112.jsp*
-// @namespace    https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/
-// @downloadURL  https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
-// @run-at       document-idle
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_deleteValue
-// ==/UserScript==
+/* === CAMPUS AUTO LOGIN v3.1 ===
+ * Usage:
+ * 1) Type your username/password or any other info into the usual text boxes.
+ * 2) Find the "Remember Me" button.
+ *    This can vary depending on the site, but its usually near the "Forgot Password" button.
+ * 3) Click it.
+ * 4) It will not automatically log in for all future visits.
+ *    If at any point your credentials changed it will detect that and you will have to reset it.
+ *
+ * Supported sites:
+ * TCI: https://student.teachtci.com/student/sign_in
+ *  - Will not sign in if you recently signed out (by design)
+ * Old Portal: https://campus.district112.org/campus/portal/isd112.jsp
+ *  - Will not sign in if you recently signed out.
+ *  - Credentials are shared between new and old portals.
+ * New Portal: https://campus.district112.org/campus/portal/students/isd112.jsp
+ *  - Credentials are shared between new and old portals.
+ *
+ * Empower support is PLANNED.
+ */
+/*
+Copyright (c) 2017 GarboMuffin
 
-// This is the transpiled TypeScript.
-// You can find the source TypeScript @ https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.ts
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+////
+// Below these lines is the raw source code.
+// Do not change it if you do not know what you're doing, you could get your account locked out.
+// Raw TypeScript: https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+// Transpiled: https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+////
+// remove keys from older versions
+GM_deleteValue("CALU"); // v3.0 username
+GM_deleteValue("CALP"); // v3.0 password
 // CONFIG
 const SUPPORT_OLD_CAMPUS = true;
 const SUPPORT_NEW_CAMPUS = true;
+const SUPPORT_TCI = true;
 // INTERNAL CONFIGS - DO NOT TOUCH
-const INTERNAL_ID = "CAL";
-const INTERNAL_UID = INTERNAL_ID + "U";
-const INTERNAL_PID = INTERNAL_ID + "P";
+var KeyNames;
+(function (KeyNames) {
+    KeyNames["CAMPUS_USERNAME"] = "CAMPUS_USERNAME";
+    KeyNames["CAMPUS_PASSWORD"] = "CAMPUS_PASSWORD";
+    KeyNames["TCI_USERNAME"] = "TCI_USERNAME";
+    KeyNames["TCI_PASSWORD"] = "TCI_PASSWORD";
+    KeyNames["TCI_TEACHER"] = "TCI_TEACHER";
+})(KeyNames || (KeyNames = {}));
 var PageState;
 (function (PageState) {
     PageState[PageState["Normal"] = 0] = "Normal";
     PageState[PageState["Error"] = 1] = "Error";
     PageState[PageState["Captcha"] = 2] = "Captcha";
 })(PageState || (PageState = {}));
+var PageType;
+(function (PageType) {
+    PageType[PageType["CampusNew"] = 0] = "CampusNew";
+    PageType[PageType["CampusOld"] = 1] = "CampusOld";
+    PageType[PageType["TCI"] = 2] = "TCI";
+})(PageType || (PageType = {}));
+var LogSeverity;
+(function (LogSeverity) {
+    LogSeverity[LogSeverity["Info"] = 0] = "Info";
+    LogSeverity[LogSeverity["Warn"] = 1] = "Warn";
+    LogSeverity[LogSeverity["Err"] = 2] = "Err";
+})(LogSeverity || (LogSeverity = {}));
 // GLOBAL METHODS
-// base64 encode
-function b64e(string) {
+function base64encode(string) {
     return btoa(string);
 }
-// base64 decode
-function b64d(string) {
+function base64decode(string) {
     return atob(string);
+}
+function getPageType() {
+    if (location.href.indexOf("student.teachtci.com/student/sign_in") > -1) {
+        return PageType.TCI;
+    }
+    else if (location.href.indexOf("campus.district112.org/campus/portal/isd112.jsp") > -1) {
+        return PageType.CampusOld;
+    }
+    else if (location.href.indexOf("campus.district112.org/campus/portal/students/isd112") > -1) {
+        return PageType.CampusNew;
+    }
+    else {
+        return null;
+    }
+}
+function log(msg, severity = LogSeverity.Info) {
+    const LOG_PREFIX = "[Campus Auto Login]";
+    if (severity === LogSeverity.Info) {
+        console.info(LOG_PREFIX, msg);
+    }
+    else if (severity === LogSeverity.Warn) {
+        console.warn(LOG_PREFIX, msg);
+    }
+    else if (severity === LogSeverity.Err) {
+        console.error(LOG_PREFIX, msg);
+    }
 }
 // IMPLEMENTING AUTO LOGIN
 class Credentials {
     constructor(username, password) {
-        // NOTE: username and password are base64 encoded
         this._username = username;
         this._password = password;
     }
     get username() {
-        return atob(this._username);
+        return base64decode(this._username);
     }
     ;
     get password() {
-        return atob(this._password);
+        return base64decode(this._password);
     }
     ;
 }
-class BaseAutoLogin {
+class TCICredentials extends Credentials {
+    constructor(username, password, teacher) {
+        super(username, password);
+        this._teacher = teacher;
+    }
+    get teacher() {
+        return base64decode(this._teacher);
+    }
+}
+// PORTAL
+class BasePortalAutoLogin {
     resetCredentials() {
-        GM_deleteValue(INTERNAL_UID);
-        GM_deleteValue(INTERNAL_PID);
+        GM_deleteValue(KeyNames.CAMPUS_USERNAME);
+        GM_deleteValue(KeyNames.CAMPUS_PASSWORD);
     }
     getCredentials() {
-        const username = GM_getValue(INTERNAL_UID, null);
-        const password = GM_getValue(INTERNAL_PID, null);
+        const username = GM_getValue(KeyNames.CAMPUS_USERNAME, null);
+        const password = GM_getValue(KeyNames.CAMPUS_PASSWORD, null);
         if (username === null || password === null) {
             return null;
         }
         return new Credentials(username, password);
     }
     setCredentials(username, password) {
-        try {
-            GM_setValue(INTERNAL_UID, b64e(username));
-            GM_setValue(INTERNAL_PID, b64e(password));
-        }
-        catch (e) {
-            alert("COULDN'T STORE CREDENTIALS, ABORTING");
-            throw e;
-        }
+        GM_setValue(KeyNames.CAMPUS_USERNAME, base64encode(username));
+        GM_setValue(KeyNames.CAMPUS_PASSWORD, base64encode(password));
     }
     getState() {
         if (document.location.href.indexOf("status=captcha") > -1) {
@@ -90,7 +167,7 @@ class BaseAutoLogin {
         }
     }
 }
-class OldPortalAutoLogin extends BaseAutoLogin {
+class OldPortalAutoLogin extends BasePortalAutoLogin {
     submit() {
         document.getElementById("signinbtn").click();
     }
@@ -123,7 +200,7 @@ class OldPortalAutoLogin extends BaseAutoLogin {
         this.setCredentials(username, password);
     }
 }
-class NewPortalAutoLogin extends BaseAutoLogin {
+class NewPortalAutoLogin extends BasePortalAutoLogin {
     submit() {
         document.getElementsByClassName("success block")[0].click();
     }
@@ -138,7 +215,7 @@ class NewPortalAutoLogin extends BaseAutoLogin {
             this.submit();
             return false;
         };
-        container.insertAdjacentElement("beforeend", el);
+        container.appendChild(el);
     }
     areCredentialsValid() {
         return document.location.href.indexOf("status=error") === -1;
@@ -156,27 +233,116 @@ class NewPortalAutoLogin extends BaseAutoLogin {
         this.setCredentials(username, password);
     }
 }
+// TCI
+class TCIAutoLogin {
+    resetCredentials() {
+        GM_deleteValue(KeyNames.TCI_USERNAME);
+        GM_deleteValue(KeyNames.TCI_PASSWORD);
+        GM_deleteValue(KeyNames.TCI_TEACHER);
+    }
+    submit() {
+        document.querySelector("input[name=commit]").click();
+    }
+    setDocumentCredentials(credentials) {
+        document.getElementById("student_username").value = credentials.username;
+        document.getElementById("student_password").value = credentials.password;
+        document.getElementById("student_teacher_email").value = credentials.teacher;
+    }
+    setCredentials(username, password, teacher) {
+        GM_setValue(KeyNames.TCI_USERNAME, base64encode(username));
+        GM_setValue(KeyNames.TCI_PASSWORD, base64encode(password));
+        GM_setValue(KeyNames.TCI_TEACHER, base64encode(teacher));
+    }
+    storeCredentials() {
+        const username = document.getElementById("student_username").value;
+        const password = document.getElementById("student_password").value;
+        const teacher = document.getElementById("student_teacher_email").value;
+        this.setCredentials(username, password, teacher);
+    }
+    onload() {
+        var el = document.createElement("a");
+        el.textContent = "Remember Me";
+        el.style.cursor = "pointer"; // tci does not give a elements a pointer so manually do it
+        el.onclick = () => {
+            this.storeCredentials();
+            this.submit();
+            return false;
+        };
+        var container = document.createElement("p");
+        container.appendChild(el);
+        document.getElementsByClassName("pos_fl")[0].appendChild(container);
+    }
+    shouldSignIn() {
+        return this.getState() === PageState.Normal && !this.hasRecentlySignedOut();
+    }
+    getState() {
+        const el = document.getElementById("flash");
+        if (el && el.innerHTML.indexOf("Incorrect username or password") > -1) {
+            return PageState.Error;
+        }
+        else {
+            return PageState.Normal;
+        }
+    }
+    hasRecentlySignedOut() {
+        const el = document.getElementById("flash");
+        if (el && el.innerHTML.indexOf("Signed out successfully") > -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    getCredentials() {
+        const username = GM_getValue(KeyNames.TCI_USERNAME, null);
+        const password = GM_getValue(KeyNames.TCI_PASSWORD, null);
+        const teacher = GM_getValue(KeyNames.TCI_TEACHER, null);
+        if (username === null || password === null || teacher === null) {
+            return null;
+        }
+        return new TCICredentials(username, password, teacher);
+    }
+}
 // ACTUALLY RUNNING THE THING
 (function () {
-    const usingNewSite = location.href.indexOf("students") > -1;
+    const pageType = getPageType();
+    // unknown page type, don't do anything
+    if (pageType === null) {
+        return;
+    }
     var loginManager;
-    if (usingNewSite) {
+    if (pageType === PageType.CampusNew) {
+        if (!SUPPORT_NEW_CAMPUS) {
+            return;
+        }
         loginManager = new NewPortalAutoLogin();
     }
-    else {
+    else if (pageType === PageType.CampusOld) {
+        if (!SUPPORT_OLD_CAMPUS) {
+            return;
+        }
         loginManager = new OldPortalAutoLogin();
+    }
+    else if (pageType === PageType.TCI) {
+        if (!SUPPORT_TCI) {
+            return;
+        }
+        loginManager = new TCIAutoLogin();
+    }
+    else {
+        return;
     }
     loginManager.onload();
     const state = loginManager.getState();
     if (state !== PageState.Normal) {
         if (state === PageState.Captcha) {
             loginManager.resetCredentials();
-            console.warn("CAPTCHA");
+            log("CAPTCHA", LogSeverity.Warn);
             alert("A captcha has been detected.\n\nYour credentials have been reset.\n\nPlease enter your credentials AND the captcha.");
         }
         else if (state === PageState.Error) {
             loginManager.resetCredentials();
-            console.warn("ERROR");
+            log("ERROR", LogSeverity.Warn);
             alert("Credentials have been detected as incorrect.\n\nThey have been reset.");
         }
     }
@@ -191,3 +357,19 @@ class NewPortalAutoLogin extends BaseAutoLogin {
         }
     }
 })();
+// the metadata block is now down here because reasons
+// ==UserScript==
+// @name         Campus Auto Login
+// @version      3.1
+// @description  Auto log-in to campus portal and other related sites.
+// @author       GarboMuffin
+// @match        https://campus.district112.org/campus/portal/isd112.jsp*
+// @match        https://campus.district112.org/campus/portal/students/isd112.jsp*
+// @match        https://student.teachtci.com/student/sign_in
+// @namespace    https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/
+// @downloadURL  https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+// @run-at       document-idle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// ==/UserScript==
--- userscripts/campus-auto-login/campus-auto-login.user.ts
+++ userscripts/campus-auto-login/campus-auto-login.user.ts
@@ -1,55 +1,117 @@
-// ==UserScript==
-// @name         Campus Auto Login
-// @version      3.0
-// @description  Auto log-in to campus portal.
-// @author       GarboMuffin
-// @match        https://campus.district112.org/campus/portal/isd112.jsp*
-// @match        https://campus.district112.org/campus/portal/students/isd112.jsp*
-// @namespace    https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/
-// @downloadURL  https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
-// @run-at       document-idle
-// @grant        GM_getValue
-// @grant        GM_setValue
-// @grant        GM_deleteValue
-// ==/UserScript==
+/* === CAMPUS AUTO LOGIN v3.1 ===
+ * Usage:
+ * 1) Type your username/password or any other info into the usual text boxes.
+ * 2) Find the "Remember Me" button.
+ *    This can vary depending on the site, but its usually near the "Forgot Password" button.
+ * 3) Click it.
+ * 4) It will not automatically log in for all future visits.
+ *    If at any point your credentials changed it will detect that and you will have to reset it.
+ * 
+ * Supported sites:
+ * TCI: https://student.teachtci.com/student/sign_in
+ *  - Will not sign in if you recently signed out (by design)
+ * Old Portal: https://campus.district112.org/campus/portal/isd112.jsp
+ *  - Will not sign in if you recently signed out.
+ *  - Credentials are shared between new and old portals.
+ * New Portal: https://campus.district112.org/campus/portal/students/isd112.jsp
+ *  - Credentials are shared between new and old portals.
+ * 
+ * Empower support is PLANNED.
+ */
 
-// This is the pre-transpiled TypeScript.
-// You can find the transpiled TypeScript @ https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+/*
+Copyright (c) 2017 GarboMuffin
 
-// Tampermonkey
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+////
+// Below these lines is the raw source code.
+// Do not change it if you do not know what you're doing, you could get your account locked out.
+// Raw TypeScript: https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+// Transpiled: https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+////
+
+// remove keys from older versions
+GM_deleteValue("CALU"); // v3.0 username
+GM_deleteValue("CALP"); // v3.0 password
+
 type GM_Value = string | number | boolean;
 declare function GM_getValue<T>(key: string, def?: T): GM_Value | T;
 declare function GM_setValue(key: string, value: GM_Value): void;
 declare function GM_deleteValue(key: string): void;
+declare function GM_addStyle(css: string): void;
 
 // CONFIG
 const SUPPORT_OLD_CAMPUS = true;
 const SUPPORT_NEW_CAMPUS = true;
+const SUPPORT_TCI = true;
 
 // INTERNAL CONFIGS - DO NOT TOUCH
-const INTERNAL_ID = "CAL";
-const INTERNAL_UID = INTERNAL_ID + "U";
-const INTERNAL_PID = INTERNAL_ID + "P";
-
+enum KeyNames {
+  CAMPUS_USERNAME = "CAMPUS_USERNAME",
+  CAMPUS_PASSWORD = "CAMPUS_PASSWORD",
+  TCI_USERNAME = "TCI_USERNAME",
+  TCI_PASSWORD = "TCI_PASSWORD",
+  TCI_TEACHER = "TCI_TEACHER",
+}
 enum PageState { Normal, Error, Captcha }
+enum PageType { CampusNew, CampusOld, TCI }
+enum LogSeverity { Info, Warn, Err }
 
 // GLOBAL METHODS
 
-// base64 encode
-function b64e(string: string) {
+function base64encode(string: string) {
   return btoa(string);
 }
 
-// base64 decode
-function b64d(string: string) {
+function base64decode(string: string) {
   return atob(string);
+}
+
+function getPageType(): PageType | null {
+  if (location.href.indexOf("student.teachtci.com/student/sign_in") > -1) {
+    return PageType.TCI;
+  } else if (location.href.indexOf("campus.district112.org/campus/portal/isd112.jsp") > -1) {
+    return PageType.CampusOld;
+  } else if (location.href.indexOf("campus.district112.org/campus/portal/students/isd112") > -1) {
+    return PageType.CampusNew;
+  } else {
+    return null;
+  }
+}
+
+function log(msg: string, severity: LogSeverity = LogSeverity.Info) {
+  const LOG_PREFIX = "[Campus Auto Login]";
+  if (severity === LogSeverity.Info) {
+    console.info(LOG_PREFIX, msg);
+  } else if (severity === LogSeverity.Warn) {
+    console.warn(LOG_PREFIX, msg);
+  } else if (severity === LogSeverity.Err) {
+    console.error(LOG_PREFIX, msg);
+  }
 }
 
 // IMPLEMENTING AUTO LOGIN
 
 class Credentials {
   constructor(username: string, password: string) {
-    // NOTE: username and password are base64 encoded
     this._username = username;
     this._password = password;
   }
@@ -57,11 +119,23 @@ class Credentials {
   private _username: string;
   private _password: string;
   get username() {
-    return atob(this._username);
+    return base64decode(this._username);
   };
   get password() {
-    return atob(this._password);
+    return base64decode(this._password);
   };
+}
+
+class TCICredentials extends Credentials {
+  constructor(username: string, password: string, teacher: string) {
+    super(username, password);
+    this._teacher = teacher;
+  }
+
+  private _teacher: string;
+  get teacher() {
+    return base64decode(this._teacher);
+  }
 }
 
 interface AutoLogin {
@@ -79,15 +153,17 @@ interface AutoLogin {
   getCredentials(): Credentials | null;
 }
 
-abstract class BaseAutoLogin implements AutoLogin {
+// PORTAL
+
+abstract class BasePortalAutoLogin implements AutoLogin {
   resetCredentials(): void {
-    GM_deleteValue(INTERNAL_UID);
-    GM_deleteValue(INTERNAL_PID);
+    GM_deleteValue(KeyNames.CAMPUS_USERNAME);
+    GM_deleteValue(KeyNames.CAMPUS_PASSWORD);
   }
 
   getCredentials(): Credentials | null {
-    const username = GM_getValue(INTERNAL_UID, null) as string | null;
-    const password = GM_getValue(INTERNAL_PID, null) as string | null;
+    const username = GM_getValue(KeyNames.CAMPUS_USERNAME, null) as string | null;
+    const password = GM_getValue(KeyNames.CAMPUS_PASSWORD, null) as string | null;
     if (username === null || password === null) {
       return null;
     }
@@ -95,13 +171,8 @@ abstract class BaseAutoLogin implements AutoLogin {
   }
 
   protected setCredentials(username: string, password: string): void {
-    try {
-      GM_setValue(INTERNAL_UID, b64e(username));
-      GM_setValue(INTERNAL_PID, b64e(password));
-    } catch (e) {
-      alert("COULDN'T STORE CREDENTIALS, ABORTING");
-      throw e;
-    }
+    GM_setValue(KeyNames.CAMPUS_USERNAME, base64encode(username));
+    GM_setValue(KeyNames.CAMPUS_PASSWORD, base64encode(password));
   }
 
   getState() {
@@ -121,7 +192,7 @@ abstract class BaseAutoLogin implements AutoLogin {
   abstract shouldSignIn(): boolean;
 }
 
-class OldPortalAutoLogin extends BaseAutoLogin {
+class OldPortalAutoLogin extends BasePortalAutoLogin {
   submit(): void {
     (document.getElementById("signinbtn") as HTMLButtonElement).click();
   }
@@ -161,7 +232,7 @@ class OldPortalAutoLogin extends BaseAutoLogin {
   }
 }
 
-class NewPortalAutoLogin extends BaseAutoLogin {
+class NewPortalAutoLogin extends BasePortalAutoLogin {
   submit(): void {
     (document.getElementsByClassName("success block")[0] as HTMLButtonElement).click();
   }
@@ -179,7 +250,7 @@ class NewPortalAutoLogin extends BaseAutoLogin {
       return false;
     };
 
-    container.insertAdjacentElement("beforeend", el);
+    container.appendChild(el);
   }
 
   areCredentialsValid() {
@@ -202,17 +273,115 @@ class NewPortalAutoLogin extends BaseAutoLogin {
   }
 }
 
+// TCI
+class TCIAutoLogin implements AutoLogin {
+  resetCredentials() {
+    GM_deleteValue(KeyNames.TCI_USERNAME);
+    GM_deleteValue(KeyNames.TCI_PASSWORD);
+    GM_deleteValue(KeyNames.TCI_TEACHER);
+  }
+
+  submit() {
+    (document.querySelector("input[name=commit]") as HTMLLinkElement).click();
+  }
+
+  setDocumentCredentials(credentials: TCICredentials) {
+    (document.getElementById("student_username") as HTMLInputElement).value = credentials.username;
+    (document.getElementById("student_password") as HTMLInputElement).value = credentials.password;
+    (document.getElementById("student_teacher_email") as HTMLInputElement).value = credentials.teacher;
+  }
+
+  setCredentials(username: string, password: string, teacher: string) {
+    GM_setValue(KeyNames.TCI_USERNAME, base64encode(username));
+    GM_setValue(KeyNames.TCI_PASSWORD, base64encode(password));
+    GM_setValue(KeyNames.TCI_TEACHER, base64encode(teacher));
+  }
+
+  storeCredentials() {
+    const username = (document.getElementById("student_username") as HTMLInputElement).value;
+    const password = (document.getElementById("student_password") as HTMLInputElement).value;
+    const teacher = (document.getElementById("student_teacher_email") as HTMLInputElement).value;
+    this.setCredentials(username, password, teacher);
+  }
+
+  onload() {
+    var el = document.createElement("a");
+    el.textContent = "Remember Me";
+    el.style.cursor = "pointer"; // tci does not give a elements a pointer so manually do it
+    el.onclick = () => {
+      this.storeCredentials();
+      this.submit();
+
+      return false;
+    }
+
+    var container = document.createElement("p");
+    container.appendChild(el);
+
+    document.getElementsByClassName("pos_fl")[0].appendChild(container);
+  }
+
+  shouldSignIn() {
+    return this.getState() === PageState.Normal && !this.hasRecentlySignedOut();
+  }
+
+  getState() {
+    const el = document.getElementById("flash");
+    if (el && el.innerHTML.indexOf("Incorrect username or password") > -1) {
+      return PageState.Error;
+    } else {
+      return PageState.Normal;
+    }
+  }
+
+  hasRecentlySignedOut() {
+    const el = document.getElementById("flash");
+    if (el && el.innerHTML.indexOf("Signed out successfully") > -1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getCredentials(): TCICredentials | null {
+    const username = GM_getValue(KeyNames.TCI_USERNAME, null) as string | null;
+    const password = GM_getValue(KeyNames.TCI_PASSWORD, null) as string | null;
+    const teacher = GM_getValue(KeyNames.TCI_TEACHER, null) as string | null;
+    if (username === null || password === null || teacher === null) {
+      return null;
+    }
+    return new TCICredentials(username, password, teacher);
+  }
+}
 
 // ACTUALLY RUNNING THE THING
 
 (function () {
-  const usingNewSite = location.href.indexOf("students") > -1;
+  const pageType = getPageType();
+
+  // unknown page type, don't do anything
+  if (pageType === null) {
+    return;
+  }
 
   var loginManager: AutoLogin;
-  if (usingNewSite) {
+  if (pageType === PageType.CampusNew) {
+    if (!SUPPORT_NEW_CAMPUS) {
+      return;
+    }
     loginManager = new NewPortalAutoLogin();
-  } else {
+  } else if (pageType === PageType.CampusOld) {
+    if (!SUPPORT_OLD_CAMPUS) {
+      return;
+    }
     loginManager = new OldPortalAutoLogin();
+  } else if (pageType === PageType.TCI) {
+    if (!SUPPORT_TCI) {
+      return;
+    }
+    loginManager = new TCIAutoLogin();
+  } else {
+    return;
   }
 
   loginManager.onload();
@@ -221,11 +390,11 @@ class NewPortalAutoLogin extends BaseAutoLogin {
   if (state !== PageState.Normal) {
     if (state === PageState.Captcha) {
       loginManager.resetCredentials();
-      console.warn("CAPTCHA");
+      log("CAPTCHA", LogSeverity.Warn);
       alert("A captcha has been detected.\n\nYour credentials have been reset.\n\nPlease enter your credentials AND the captcha.");
     } else if (state === PageState.Error) {
       loginManager.resetCredentials();
-      console.warn("ERROR");
+      log("ERROR", LogSeverity.Warn);
       alert("Credentials have been detected as incorrect.\n\nThey have been reset.");
     }
   }
@@ -241,3 +410,20 @@ class NewPortalAutoLogin extends BaseAutoLogin {
     }
   }
 })();
+
+// the metadata block is now down here because reasons
+// ==UserScript==
+// @name         Campus Auto Login
+// @version      3.1
+// @description  Auto log-in to campus portal and other related sites.
+// @author       GarboMuffin
+// @match        https://campus.district112.org/campus/portal/isd112.jsp*
+// @match        https://campus.district112.org/campus/portal/students/isd112.jsp*
+// @match        https://student.teachtci.com/student/sign_in
+// @namespace    https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/
+// @downloadURL  https://garbomuffin.bitbucket.io/userscripts/campus-auto-login/campus-auto-login.user.js
+// @run-at       document-idle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// ==/UserScript==
