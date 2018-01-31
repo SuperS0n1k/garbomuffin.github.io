4d0d1e8f8a54560db887ad0d82ef440be4d1bf38
GarboMuffin <tommyweber33@gmail.com>
2018-01-30 18:15:40 -0600

    ytembed userscript v0.0.2

--- ytembed/userscript/ytembed.user.js
+++ ytembed/userscript/ytembed.user.js
@@ -1,5 +1,71 @@
+/*** CHANGELOG ***/
+
+/*
+ytembed userscript v0.0.2:
+ * config page: https://garbomuffin.github./ytembed/userscript/config.html
+ * hide links config
+   * links still functionally the same, but hidden unless you hover over them
+ * disable on new/old youtube layouts
+*/
+
+// ==UserScript==
+// @name         ytembed
+// @namespace    https://garbomuffin.github.io/ytembed/
+// @version      0.0.2
+// @author       GarboMuffin
+// @match        https://www.youtube.com/*
+// @match        https://garbomuffin.github./ytembed/userscript/config.html
+// @downloadURL  https://garbomuffin.github.io/ytembed/userscript/ytembed.user.js
+// @run-at       document-idle
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+// ==/UserScript==
+
 (function() {
+  "use strict";
   const YTEMBED_CONTAINER_ID = "_ytembed_container";
+
+  //
+  // INIT CONFIG
+  //
+
+  GM_config.init({
+    id: "YTEmbedConfig",
+    title: "Settings",
+    fields: {
+      // Main
+      classicSupport: {
+        label: "Support classic YouTube design",
+        type: "checkbox",
+        default: true,
+        section: ["Main", "These are the only things you should change if you want to."],
+      },
+      materialSupport: {
+        label: "Support new(ish) YouTube design",
+        type: "checkbox",
+        default: true,
+      },
+
+      // Experimental
+      hideLinks: {
+        label: "Hide Links (please ignore this setting)",
+        type: "checkbox",
+        default: false,
+        section: ["Experimental", "Please ignore all of these."],
+      },
+      checkInterval: {
+        label: "Interval time (ms)",
+        type: "int",
+        default: 100,
+      },
+    },
+  });
+  
+  //
+  // UTIL METHODS
+  //
 
   function getElementById(id) {
     const el = document.getElementById(id);
@@ -15,12 +81,42 @@
       const val = opts[key];
       el[key] = val;
     }
+    el.classList.add("ytembed");
     return el;
   }
 
-  class Handler {
+  //
+  // HANDLERS
+  //
+
+  class AbstractHandler {
+    // must be implemented when extended
+    run() {
+      console.warn("ytembed: unimplemented run()");
+    }
+  }
+
+  class ConfigHandler extends AbstractHandler {
+    run() {
+      GM_config.open();
+    }
+  }
+
+  class VideoHandler extends AbstractHandler {
+    // must be implemented when extended
     isVideoBlocked() {
+      console.warn("ytembed: unimplemented isVideoBlocked()");
       return false;
+    }
+
+    // must be implemented when extended
+    addBlockedLink() {
+      console.warn("ytembed: unimplemented addBlockedLink()");
+    }
+
+    // must be implemented when extended
+    addVideoLink() {
+      console.warn("ytembed: unimplemented addVideoLink()");
     }
 
     run() {
@@ -48,17 +144,9 @@
     canRun() {
       return !document.getElementById(YTEMBED_CONTAINER_ID);
     }
-
-    addBlockedLink() {
-      console.warn("no defined addBlockedLink()");
-    }
-
-    addVideoLink() {
-      console.warn("no defined addVideoLink()");
-    }
   }
 
-  class ClassicHandler extends Handler {
+  class ClassicHandler extends VideoHandler {
     isVideoBlocked() {
       const el = document.getElementById("unavailable-message");
       if (!el) {
@@ -94,7 +182,7 @@
     }
   }
 
-  class MaterialHandler extends Handler {
+  class MaterialHandler extends VideoHandler {
     isVideoBlocked() {
       const el = document.getElementsByTagName("ytd-playability-error-supported-renderers")[0];
       if (!el) {
@@ -135,13 +223,21 @@
   }
 
   function getHandlerClass() {
-    const usingMaterial = !!document.getElementById("polymer-app");
-    if (usingMaterial) {
+    const onConfigPage = location.href.indexOf("/ytembed/userscript/config.html") > -1;
+    const isUsingMaterial = !!document.getElementById("polymer-app");
+    if (onConfigPage) {
+      return ConfigHandler;
+    } else if (isUsingMaterial && GM_config.get("materialSupport")) {
       return MaterialHandler;
-    } else {
+    } else if (GM_config.get("classicSupport")) {
       return ClassicHandler;
     }
+    return null;
   }
+
+  //
+  // CHOOSING A HANDLER AND RUNNING IT
+  //
 
   const handlerClass = getHandlerClass();
   if (!handlerClass) {
@@ -149,19 +245,23 @@
   }
   const handler = new handlerClass();
 
-  const runningInterval = setInterval(function() {
-    if (handler.canRun()) {
-      handler.run();
-    }
-  }, 100);
-}());
+  // experimental: hide links option
+  // makes them invisible however functionally the same if you know where to click
+  if (GM_config.get("hideLinks")) {
+    // opacity: 0 makes it hidden but still functional and take up space
+    GM_addStyle("a.ytembed{opacity: 0;}");
+    // when hovered show
+    GM_addStyle("a.ytembed:hover{opacity: 1;}");
+  }
 
-// ==UserScript==
-// @name         ytembed
-// @namespace    https://garbomuffin.github.io/ytembed/
-// @version      0.0.1
-// @author       GarboMuffin
-// @match        https://www.youtube.com/*
-// @downloadURL  https://garbomuffin.github.io/ytembed/userscript/ytembed.user.js
-// @run-at       document-idle
-// ==/UserScript==
+  // VideoHandlers are special in that they constantly need to be running
+  if (handler instanceof VideoHandler) {
+    setInterval(function() {
+      if (handler.canRun()) {
+        handler.run();
+      }
+    }, GM_config.get("checkInterval"));
+  } else {
+    handler.run();
+  }
+}());
